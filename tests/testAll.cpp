d51f444b9a87bf8e50f489ec7c066f7586465abf
jaguardeer <pdeloz@gmail.com>
2022-04-18 11:27:09 -0500

    test(testAll): make CreateVulkanInstance use el::Result

--- tests/testAll.cpp
+++ tests/testAll.cpp
@@ -5,6 +5,7 @@
 // engineLibrary headers
 #include <el/Window.hpp>
 #include <el/VkResultString.hpp>
+#include <el/util.hpp>
 
 using std::cout;
 using std::endl;
@@ -16,7 +17,37 @@ auto PrintVulkanResult(VkResult result, const char* extra_message = nullptr) {
 	cout << "(" << result << "): " << GetVkResultString(result) << endl;
 }
 
-void CreateVulkanInstance() {
+class Instance {
+	public:
+
+	static Result<Instance, VkResult> Create(VkInstanceCreateInfo createInfo) {
+		VkInstance id = VK_NULL_HANDLE;
+		auto error = vkCreateInstance(&createInfo, nullptr, &id);
+		return {Instance(id), error};
+	}
+
+	// move assign
+	Instance& operator= (Instance&& other) { this->id = other.id; other.id = VK_NULL_HANDLE; return *this; }
+	// move construct
+	Instance (Instance &&other) : id(other.id) { other.id = VK_NULL_HANDLE; }
+
+	~Instance() {
+		cout << "destructing instance" << endl;
+		if(id != VK_NULL_HANDLE) vkDestroyInstance(id, nullptr);
+	}
+
+	explicit Instance(const Instance&) { cout << "copied an Instance :(" << endl; }// = default;//delete;
+	Instance& operator= (const Instance&) { cout << "copied an Instance :(" << endl; return *this; }// = //default;//delete;
+
+	private:
+	// non copyable
+	// 
+	explicit Instance(const VkInstance id) : id{id} {}
+
+	VkInstance id;
+};
+
+Result<Instance, VkResult> CreateVulkanInstance() {
 	static constexpr VkApplicationInfo app_info = {
 		.sType               = VK_STRUCTURE_TYPE_APPLICATION_INFO,
 		.pApplicationName    = "test app",
@@ -32,27 +63,34 @@ void CreateVulkanInstance() {
 		.pApplicationInfo = &app_info,
 	};
 
-	VkInstance vk_instance;
-	VkResult result = vkCreateInstance(&instance_info, nullptr, &vk_instance);
-	if(result != VK_SUCCESS) PrintVulkanResult(result, "failed to create Vulkan instance");
+	const auto result = Instance::Create(instance_info);
+	return result;
 }
 
+
 int main() {
-	cout << "running app..." << endl;
+	auto msg = [](const char* m) { cout << m << endl; };
+	{
+		msg("running app...");
 
-	cout << "making main window..." << endl;
-	Window gameWindow;
-	gameWindow.initWindow();
+		msg("making main window...");
+		Window gameWindow;
+		gameWindow.initWindow();
 
-	cout << "creating vulkan instance..." << endl;
-	CreateVulkanInstance();
+		msg("creating vulkan instance...");
+		const auto [instance, error] = CreateVulkanInstance();
+		if(error != VK_SUCCESS) {
+			PrintVulkanResult(error, "failed to create vulkan instance");
+		}
 
-	cout << "main loop..." << endl;
-	while(gameWindow.isOpen()) {
-		gameWindow.processMessages();
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		msg("main loop...");
+		while(gameWindow.isOpen()) {
+			gameWindow.processMessages();
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		}
+
+		msg("cleaning up...");
 	}
-
-	cout << "done!" << endl;
+	msg("done!");
 	return 0;
 }
